834948d2f56441de65ffac4f706490fcff8af3b0
Denitsa Dobreva <denitsa.dr.dobreva@gmail.com>
2025-12-05 17:05:17 +0200

    header structure

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,6 @@
 import "./globals.css";
 import { inter } from "./ui/fonts";
+import Link from "next/link";
 
 export default function RootLayout({
   children,
@@ -8,7 +9,28 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={`${inter.className} antialiased`}>{children}</body>
+      <body className={`${inter.className} antialiased`}>
+        <header className="flex justify-between items-center p-4 bg-gray-200">
+          <Link href="/">
+            <h2>Local Events Hub</h2>
+          </Link>
+
+          <nav>
+            <ul className="flex gap-4">
+              <li>
+                <Link href="/">Home</Link>
+              </li>
+              <li>
+                <Link href="/events">Events</Link>
+              </li>
+              <li>
+                <Link href="/events/new">Add New Event</Link>
+              </li>
+            </ul>
+          </nav>
+        </header>
+        <main>{children}</main>
+      </body>
     </html>
   );
 }
