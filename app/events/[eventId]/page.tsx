e1428e9158265f1ce64f2fd70036388db30d67c9
Denitsa Dobreva <denitsa.dr.dobreva@gmail.com>
2025-12-05 16:50:49 +0200

    initial file structure

---
+++ app/events/[eventId]/page.tsx
@@ -0,0 +1,8 @@
+export default function EventPage() {
+  return (
+    <div>
+      <h1>Event Details</h1>
+      <p>This page will display details for a specific event.</p>
+    </div>
+  );
+}
---
+++ app/events/new/page.tsx
@@ -0,0 +1,8 @@
+export default function NewEventPage() {
+  return (
+    <div>
+      <h1>New event</h1>
+      <p>On this page you can add a new event.</p>
+    </div>
+  );
+}
