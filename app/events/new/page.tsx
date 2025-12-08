bc4bf7ae5facd26fde391852e9b0872ed4bc19fb
Denitsa Dobreva <denitsa.dr.dobreva@gmail.com>
2025-12-08 19:28:00 +0200

    add new event

--- app/events/new/page.tsx
+++ app/events/new/page.tsx
@@ -1,8 +1,42 @@
-export default function NewEventPage() {
+"use server";
+
+import { sql } from "../../lib/db";
+import { redirect } from "next/navigation";
+
+export async function addNewEvent(formData: FormData) {
+  const title = formData.get("title") as string;
+  const description = formData.get("description") as string;
+  const event_date = formData.get("event_date") as string;
+  const newEvent =
+    await sql`INSERT INTO events (title, description, event_date) VALUES (${title}, ${description}, ${event_date}) RETURNING *`;
+  if (newEvent.length === 0) {
+    throw new Error("Failed to add new event");
+  } else {
+    console.log("New event added:", newEvent);
+    redirect("/events");
+  }
+}
+
+export default async function NewEventPage() {
   return (
     <div>
-      <h1>New event</h1>
+      <h1>Add New Event</h1>
       <p>On this page you can add a new event.</p>
+      <form action={addNewEvent} className="flex flex-col gap-4">
+        <div>
+          <label htmlFor="title">Title:</label>
+          <input type="text" id="title" name="title" required />
+        </div>
+        <div>
+          <label htmlFor="description">Description:</label>
+          <textarea id="description" name="description" required></textarea>
+        </div>
+        <div>
+          <label htmlFor="event_date">Event Date:</label>
+          <input type="date" id="event_date" name="event_date" required />
+        </div>
+        <button type="submit">Add Event</button>
+      </form>
     </div>
   );
 }
