9c5d536fadec8004f49a8cfabb4e5d51eae1dc86
Denitsa Dobreva <denitsa.dr.dobreva@gmail.com>
2026-02-05 13:06:44 +0200

    chore(auth): NextAuth config

--- auth.ts
+++ auth.ts
@@ -1,6 +1,69 @@
 import NextAuth from "next-auth";
+import { PrismaAdapter } from "@auth/prisma-adapter";
+import { prisma } from "@/lib/db/prisma";
+import CredentialsProvider from "next-auth/providers/credentials";
+import { compareSync } from "bcrypt-ts-edge";
 
 export const { handlers, signIn, signOut, auth } = NextAuth({
-  providers: [],
+  providers: [
+    CredentialsProvider({
+      name: "Credentials",
+      credentials: {
+        email: {
+          label: "Email",
+          type: "email",
+          placeholder: "your-email@example.com",
+        },
+        password: { label: "Password", type: "password" },
+      },
+      async authorize(credentials) {
+        if (credentials == null) {
+          return null;
+        }
+        const user = await prisma.user.findUnique({
+          where: { email: credentials.email as string },
+        });
+
+        if (!user) {
+          return null;
+        }
+
+        const isMatch = compareSync(
+          credentials.password as string,
+          user.password,
+        );
+
+        if (isMatch) {
+          return {
+            id: user.id,
+            name: user.name,
+            email: user.email,
+            role: user.role,
+          };
+        }
+
+        return null;
+      },
+    }),
+  ],
   debug: true,
+  pages: {
+    signIn: "/auth/signin",
+    signOut: "/auth/signout",
+    error: "/auth/error", // Error code passed in query string as ?error=
+    verifyRequest: "/auth/verify-request", // (used for check email message)
+  },
+  session: { strategy: "jwt", maxAge: 30 * 24 * 60 * 60 /* 30 days */ },
+  adapter: PrismaAdapter(prisma),
+  callbacks: {
+    async session({ session, user, trigger, token }) {
+      session.user.id = token.sub ?? "";
+
+      if (trigger === "update" && user) {
+        session.user.name = user.name;
+      }
+
+      return session;
+    },
+  },
 });
